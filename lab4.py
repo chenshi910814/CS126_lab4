5040e7b5463b698098a2dd3566f118bd08b94a75
Shi Chen <chenshi910814@icloud.com>
2020-10-13 23:23:03 -0700

    define functions

--- lab4.py
+++ lab4.py
@@ -1,38 +1,68 @@
-#lab 4
-#Ammon Plerson
-#Shi Chen, sc2592@nau.edu
+# lab 4
+# Ammon Plerson
+# Shi Chen, sc2592@nau.edu
+import random
 
-#create a 5*5 board
-def square():
-    board = []
-    square = ['white square','black square']
-square=[
-[1,2,3],
-[3,4,5],
-[6,8,9],
-import random 
+WHITE_SQUARE = "\N{WHITE SQUARE}"
+BLACK_SQUARE = "\N{BLACK SQUARE}"
 
-#escape code
-print("\N{white square}")
 
-for _ in square: 
-    print(_)
+def randomize(board):
+    for i in range(5):
+        for j in range(5):
+            board[i][j] = random.choice([True, False])
 
-#input function
-please chose a row number(0-4)
-please choose a column number (0-4)
-You won with ... moves!
 
-#define the main function 
+def is_solved(board):
+    for row in board:
+        for square in row:
+            if not square:
+                return False
+    return True
+
+
+def solicit_row_and_col():
+    row = int(input("Please choose a row number (0-4): "))
+    column = int(input("Please choose a column number (0-4):"))
+    return row, column
+
+
+def show(board):
+    for row in board:
+        row_string = " ".join(
+            [WHITE_SQUARE if square else BLACK_SQUARE for square in row]
+        )
+        print(row_string)
+
+
+def touch(board, row, col):
+    board[row][col] = not board[row][col]
+    if row > 0:
+        board[row - 1][col] = not board[row - 1][col]
+
+    if col > 0:
+        board[row][col - 1] = not board[row][col - 1]
+
+    if row < 4:
+        board[row + 1][col] = not board[row + 1][col]
+
+    if col < 4:
+        board[row][col + 1] = not board[row][col + 1]
+
+
+# define the main function
 def main():
-    board = [...]
+    board = [[True] * 5 for i in range(5)]
 
     randomize(board)
-    moves = 0 
+    moves = 0
     while not is_solved(board):
         show(board)
-        (row,col) = solicit_row_and_col()
+        (row, col) = solicit_row_and_col()
         touch(board, row, col)
         moves += 1
     show(board)
     print(f"You won with {moves} moves!")
+
+if __name__ == "__main__":
+    main()
